eaa603cbf896a49c7fd7b2d77e8f680cf6b9039a
Maciej Sikorski <maciejsikorski00@gmail.com>
2017-12-28 23:16:21 +0100

    Hellow World

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let app = express();
 
 let port = 5000;
 
+app.get('/', (req, res) => res.send('Hellow World!'));
+
 app.listen(port, (err) => {
     console.log('Running server on port ', port);
 });
