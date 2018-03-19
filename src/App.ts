a3dc08533ac58114aec3833e3f01a849070ae2f5
Maciek <maciej.sikorski@valueadd.pl>
2018-03-19 18:29:02 +0100

    removed bad handler

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,8 @@
-import * as express from 'express'
-import {Application} from 'express'
+import { json } from 'body-parser';
+import * as express from 'express';
+import { Application } from 'express';
 import * as mongoose from 'mongoose';
-import {Router} from "./routes/Router";
-import {json} from "body-parser";
-import clientErrorHandler from "./handlers/ClientErrorHandler";
-
+import { Router } from './routes/Router';
 
 class App {
     public app: Application;
@@ -13,25 +11,13 @@ class App {
         this.app = express();
         mongoose.connect('mongodb://localhost:27017/jewish-app');
         this.app.use(json());
-        let app = this.app;
-
-        // this.mountRoutes();
+        this.mountRoutes();
         this.app.use('/', Router);
-        this.registerHandlers();
-        this.app.use(function(error, req, res, next) {
-            // Any request to this server will get here, and will send an HTTP
-            // response with the error message 'woops'
-            res.json({ message: error.message });
-        });
     }
 
     private mountRoutes(): void {
         this.app.use('/', Router);
-    }
 
-    private registerHandlers(): void {
-        this.app.use(clientErrorHandler);
-    }
 }
 
 export default new App().app
